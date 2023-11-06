6ba8e3d3fc7d1e58d6d036982c601b8b44277818
David Vanderhaar <d.vanderhaarhunter@gmail.com>
2023-11-06 08:43:44 -0600

    WIP tourney screen

--- src/nystrum/Modes/HiddenLeaf/Screen/Tournament.js
+++ src/nystrum/Modes/HiddenLeaf/Screen/Tournament.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { CARTRIDGE } from '../../../Nystrum';
 import { SCREENS } from '../../../Modes/HiddenLeaf/Screen/constants';
+import { Player } from '../../../Entities';
 
 export default function Tournament(props) {
+  const characters = props.characters.map((character) => character.basicInfo)
+  const player = props.selectedCharacter.basicInfo
   return (
     <div className="Title">
       <div
@@ -13,6 +16,9 @@ export default function Tournament(props) {
           backgroundColor: CARTRIDGE.theme.main,
         }}
       >
+        <h1>Tournament</h1>
+        <PlayerCard character={player} />
+        <OpponentLineup characters={characters} />
         <button
           className='btn btn-main btn-themed'
           onClick={() => props.setActiveScreen(SCREENS.LEVEL)}
@@ -22,4 +28,104 @@ export default function Tournament(props) {
       </div>
     </div>
   );
+}
+
+function OpponentLineup({characters, player}) {
+  return (
+    <div
+      className='opponent-lineup'
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+      }}
+    >
+      {
+        characters.map((character, index) => {
+          return (
+            <OpponentCard 
+              key={index}
+              character={character} 
+            />
+          )
+        })
+      }
+    </div>
+  )
+}
+
+function OpponentCard({character}) {
+  return (
+    <div
+      className='opponent-card'
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        // width: '100%',
+        border: '3px solid ',
+        borderRadius: 5,
+        borderColor: character.renderer.color,
+        padding: 5,
+        margin: 5,
+      }}
+    >
+      <img
+        src={character.portrait}
+        alt={character.name}
+        style={{
+          height: '100%',
+          width: '100%',
+        }}
+      />
+      <div
+        className='opponent-card__name'
+        style={{
+          color: character.renderer.color,
+          backgroundColor: character.renderer.background,
+        }}
+      >
+        {character.name}
+      </div>
+    </div>
+  )
+}
+
+function PlayerCard({character}) {
+  return (
+    <div
+      className='player-card'
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 200,
+        border: '3px solid ',
+        borderRadius: 5,
+        borderColor: character.renderer.color,
+      }}
+    >
+      <img
+        src={character.portrait}
+        alt={character.name}
+        style={{
+          height: '100%',
+          width: '100%',
+        }}
+      />
+      <div
+        className='player-card__name'
+        style={{
+          color: character.renderer.color,
+          backgroundColor: character.renderer.background,
+        }}
+      >
+        {character.name}
+      </div>
+    </div>
+  )
 }
