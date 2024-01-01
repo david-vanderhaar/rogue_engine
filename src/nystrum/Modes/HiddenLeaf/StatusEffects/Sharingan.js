44a37d458920dab0760528c7b4f2cf68438190d5
David Vanderhaar <d.vanderhaarhunter@gmail.com>
2023-12-31 19:22:58 -0600

    sharingan can now use opponentes moves

--- src/nystrum/Modes/HiddenLeaf/StatusEffects/Sharingan.js
+++ src/nystrum/Modes/HiddenLeaf/StatusEffects/Sharingan.js
@@ -3,15 +3,16 @@ import SpatterEmitter from '../../../Engine/Particle/Emitters/spatterEmitter'
 import { COLORS as HIDDEN_LEAF_COLORS } from '../../HiddenLeaf/theme';
 
 export class Sharingan extends Base {
-  constructor({speedBuff = 200, damageBuff = 2, chakraCost = 1, ...args}) {
+  constructor({speedBuff = 0, damageBuff = 0, chakraCost = 1, ...args}) {
     super({ ...args });
     this.name = 'activate your birthright, your clan\'s kekkei genkai, the sharingan.';
-    this.description = "at the cost of your chakra, you can see your opponent's moves. This allows you to move faster and hit harder."
+    this.description = "at the cost of your chakra, you can see your opponent's moves."
     this.allowDuplicates = false
     this.processOnlyOnPlayerTurn = true
     this.lifespan = -1
     this['actor_background'] = this.actor.renderer.background;
     this['actor_color'] = this.actor.renderer.color;
+    this['actor_inialize_keymap'] = this.actor.initializeKeymap
     this.renderer = {
       color: HIDDEN_LEAF_COLORS.black,
       background: HIDDEN_LEAF_COLORS.sasuke_alt,
@@ -25,6 +26,12 @@ export class Sharingan extends Base {
       this.actor.energy += speedBuff;
       this.actor.attackDamage += damageBuff;
       this.game.addMessage(`${this.actor.name} activated the sharingan`, {color: this.renderer.color, backgroundColor: this.renderer.background})
+
+      // get enemy
+      const opp = this.game.mode.getTournamentOpponent().initialize(this.game.engine)
+      // swap keymaps
+      this.actor.initializeKeymap = opp.initializeKeymap
+      this.actor.reinitializeKeymap()
     }
 
     this.onStop = () => {
@@ -33,6 +40,9 @@ export class Sharingan extends Base {
       this.actor.speed -= speedBuff;
       this.actor.attackDamage -= damageBuff;
       this.game.addMessage(`${this.actor.name} deactivated the sharingan`, {color: this.renderer.color, backgroundColor: this.renderer.background})
+
+      this.actor.initializeKeymap = this['actor_inialize_keymap']
+      this.actor.reinitializeKeymap()
     }
 
     this.onStep = (timePassed) => {
