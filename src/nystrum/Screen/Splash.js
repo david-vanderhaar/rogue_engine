5b67888df88e10021b46e53da51d5ac6ea8339b2
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2025-05-21 21:07:43 -0500

    hidden leaf: splash screen;

--- src/nystrum/Screen/Splash.js
+++ src/nystrum/Screen/Splash.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CARTRIDGE } from '../Nystrum';
 import { SCREENS } from './constants';
 
 export default function Splash(props) {
-    return (
-      <div className="Title">
-        <div className="Title__content">
-          <h2 style={{color: CARTRIDGE.theme.accent, marginBottom: 70}}>Splash Screen</h2>
-          <button
-            className='btn btn-main btn-themed'
-            onClick={() => {
-              props.setActiveScreen(SCREENS.TITLE)
-            }}
-          >
-            It's my ninja way!
-          </button>
+  function nextScreen () {
+    props.setActiveScreen(SCREENS.TITLE)
+  }
+
+  useEffect(() => {
+    // Add event listener when the component mounts
+    window.addEventListener('click', nextScreen);
+
+    // Clean up by removing the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('click', nextScreen);
+    };
+  }, []);
+
+  return (
+    <div className="Title" style={{height: 720, width: 1280, backgroundColor: 'var(--color-console-background)'}}>
+      <div className="Title__content">
+        <img
+          src={window.PUBLIC_URL + '/hidden_leaf/title/exclaim_0.png'}
+          alt="Exclaim 0"
+          style={{left: 446, top: 96}}
+          className="fadeIn__image"
+        />
+        <div
+          className='btn btn-main btn-themed-outline Splash__button'
+          style={{top: 600, position: 'relative'}}
+          onClick={nextScreen}
+        >
+          click to start
         </div>
       </div>
-    );
+    </div>
+  );
 }
