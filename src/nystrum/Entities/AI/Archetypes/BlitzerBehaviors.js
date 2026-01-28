5d83e149757130b4efe5dbbef12f56e0ec763383
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2026-01-27 20:53:11 -0600

    hidden leaf: AI phases;

--- src/nystrum/Entities/AI/Archetypes/BlitzerBehaviors.js
+++ src/nystrum/Entities/AI/Archetypes/BlitzerBehaviors.js
@@ -1,7 +1,8 @@
 import * as Constant from '../../../constants';
 import * as Behaviors from '../Behaviors';
 
-function BlitzerBehaviors(basicInfo) {
+// Phase 1: Close distance and attack
+function FollowAndAttack(basicInfo) {
   return [
     new Behaviors.MoveTowardsEnemy({
       repeat: basicInfo.speed/Constant.ENERGY_THRESHOLD,
@@ -15,6 +16,41 @@ function BlitzerBehaviors(basicInfo) {
     }),
     new Behaviors.ExecuteAttack({repeat: 1}),
   ]
+}
+
+// Phase 2: Special Move
+function SpecialMove(basicInfo) {
+  return [
+    new Behaviors.MoveAwayFromEnemy({
+      repeat: 10,
+      maintainDistanceOf: 10,
+    }),
+  ]
+}
+
+// Phase 3: Apply Light Power Up
+function LightPowerUp(basicInfo) {
+  return [
+  ]
+}
+
+// Phase 4: Apply Full Power Up (Inner Gates)
+function FullPowerUp(basicInfo) {
+  return [
+  ]
+}
+
+function BlitzerBehaviors(basicInfo) {
+  return [
+    // Phase 1: Close distance and attack
+    ...Array(2).fill(FollowAndAttack(basicInfo)).flat(),
+    // Phase 2: Special Move
+    ...Array(1).fill(SpecialMove(basicInfo)).flat(),
+    // Phase 3: Apply Light Power Up
+    // ...Array(1).fill(LightPowerUp(basicInfo)).flat(),
+    // Phase 4: Apply Full Power Up (Inner Gates)
+    // ...Array(1).fill(FullPowerUp(basicInfo)).flat(),
+  ];
 }
 
 export default BlitzerBehaviors;
